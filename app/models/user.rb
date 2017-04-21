9834a8bdcf4496bc17ef5eaafdeeae806e98188f
Toshiaki Harada <t.harada@nsl.cs.waseda.ac.jp>
2017-04-21 01:33:09 +0000

    favorite method OK

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
 
+  has_many :favorites
+  has_many :favoritelist, through: :favorites, source: :micropost
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :favoriters, through: :reverses_of_favorite, source: :user
 
   def follow(other_user)
     unless self == other_user
@@ -30,4 +34,18 @@ class User < ApplicationRecord
     Micropost.where(user_id: self.following_ids + [self.id])
   end
 
+  def favorite(other_post)
+    unless self == other_post.user
+      self.favorites.find_or_create_by(micropost_id: other_post.id)
+    end
+  end
+
+  def unfavorite(other_post)
+    favorite = self.favorites.find_by(micropost_id: other_post.id)
+    favorite.destroy if favorite
+  end
+
+  def favoritelist?(other_post)
+    self.followings.include?(other_user)
+  end
 end
